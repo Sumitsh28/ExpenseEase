dc7421610b55cd409ce99ccb2acbae1072473f7f
Sumitsh28 <sumit.off28@gmail.com>
2024-08-12 22:41:47 +0530

    Done

---
+++ app/auth/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import Image from "next/image";
+import { getRandomItem } from "@/utils/get-random-item";
+import quotes from "../../public/data/quotes.json";
+import authImages from "../../public/data/auth-images.json";
+
+export const metadata: Metadata = {
+  title: "Expense Ease - Authentication",
+  description: "Signin or signup into Expense Ease",
+};
+
+export default async function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const quoteItem = getRandomItem(quotes);
+  const imagePath = getRandomItem(authImages);
+
+  return (
+    <div className="relative flex-col items-center justify-center h-screen md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative flex-col hidden h-full p-10 text-white dark:border-r lg:flex">
+        <div
+          className="absolute inset-0 bg-zinc-900"
+          style={{
+            backgroundImage: `url('${imagePath}')`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <div className="relative z-20 flex items-center text-lg font-medium gap-x-2 p-1.5 bg-black bg-opacity-50 rounded-lg w-[203px]">
+          <Image
+            src="/images/logo.webp"
+            alt="Expense Ease logo"
+            width={45}
+            height={45}
+          />
+          <span className="">Expense Ease</span>
+        </div>
+      </div>
+      <div className="flex items-center h-full p-4 lg:p-8">{children}</div>
+    </div>
+  );
+}
